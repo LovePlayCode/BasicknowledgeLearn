8403225b3dde323c93735d41280b5abbe4aca27e
yyxwan1314 <1244224501@qq.com>
2024-10-27 11:49:39 +0800

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+
+// 装饰器函数
+function sealed(target: any) {
+  console.log('target===',target)
+  // do something with 'target' ...
+}
+@sealed()
+function main(a:any,b:any){
+  return a + b
+}
+console.log(main(10,20))
