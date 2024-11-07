347bd7b444eaed5f7607be139f2f560fd1265809
lhl <1244224501@qq.com>
2024-11-07 22:48:52 +0800

    feat: 实现深拷贝

--- HTMLLearn/高阶函数/实现深拷贝.js
+++ HTMLLearn/高阶函数/实现深拷贝.js
@@ -51,6 +51,38 @@ function cloneReg(targe) {
     return result;
 }
 
+/**
+ * 克隆函数
+ * 1. 预定义一些正则表达式，分别可取出函数体和函数参数中的内容
+ * 2. 定义参数表达式
+ * 3. 通过 toString 取到函数的字符串定义
+ * 4. 箭头函数没有prototype,可以通过这个判断是普通函数还是箭头函数
+ * 5. 先处理body，然后判断有没有params 由的话由 Function 创建
+ */
+function cloneFunction(func){
+    const bodyReg = /(?<={)(.|\n)+(?=})/m;
+    const paramReg = /(?<=\().+(?=\)\s+{)/;
+    const funcToString = func.toString()
+    if(func.prototype){
+        const body = bodyReg.exec(funcToString)
+        const params = paramReg.exec(funcToString)
+        if(body){
+            if(params){
+                return new Function(...params[0].split(','),body[0])
+            }else{
+                return new Function(body[0])
+            }
+        }else{
+            return null
+        }
+    }else{
+        return eval(func)
+    }
+}
+
+
+
+
 //   const getType = Object.prototype.toString.call(obj);
 const canTraverse = {
   "[object Map]": true,
@@ -85,8 +117,35 @@ const numberTag = "[object Number]";
 const regexpTag = "[object RegExp]";
 const stringTag = "[object String]";
 const symbolTag = "[object Symbol]";
+const funcTag = '[object Function]'
 // 可以遍历的数据类型
 const deepTag = [mapTag, setTag, arrayTag, objectTag, argsTag];
+
+
+/**
+ * 根据不同的类型创建不同的实例
+ */
+function cloneOtherType(targe,type){
+    const Ctor = targe.constructor
+    switch(type){
+        case boolTag:
+        case numberTag:
+        case stringTag:
+        case errorTag:
+        case dateTag:
+            return new Ctor(targe)
+        case regexpTag:
+            return cloneReg(targe)
+        case symbolTag:
+            return cloneSymbol(targe)
+        case funcTag:
+            return cloneFunction(targe)
+        default: 
+            return null                            
+    }
+}
+
+
 
 /**
  * 1. map 中是否有值，有值就返回target
@@ -139,6 +198,8 @@ function clone(target, map = new WeakMap()) {
   
     if (deepTag.includes(type)) {
       cloneObj = getInit(target);
+    }else{
+        return cloneOtherType(target,type)
     }
     
   
@@ -169,15 +230,50 @@ function clone(target, map = new WeakMap()) {
   }
 }
 
-const aa1 = new Map();
-aa1.set("Jessie", { phone: "213-555-1234", address: "123 N 1st Ave" });
+// const aa1 = new Map();
+// aa1.set("Jessie", { phone: "213-555-1234", address: "123 N 1st Ave" });
 
-console.log(aa1);
+// console.log(aa1);
 
-const a = { name: "lhl", age: 20, ccc: aa1 };
-a.target = a;
-const newObj = deepClone(a);
-const newObj2 = clone(a);
-a.ccc.set("Jessie", 2);
-a.age = 100;
-console.log("newObj==", newObj, newObj2);
+// const a = { name: "lhl", age: 20, ccc: aa1 };
+// a.target = a;
+// const newObj = deepClone(a);
+// const newObj2 = clone(a);
+// a.ccc.set("Jessie", 2);
+// a.age = 100;
+// console.log("newObj==", newObj, newObj2);
+const map = new Map();
+map.set('key', 'value');
+map.set('ConardLi', 'code秘密花园');
+
+const set = new Set();
+set.add('ConardLi');
+set.add('code秘密花园');
+
+const target = {
+    field1: 1,
+    field2: undefined,
+    field3: {
+        child: 'child'
+    },
+    field4: [2, 4, 8],
+    empty: null,
+    map,
+    set,
+    bool: new Boolean(true),
+    num: new Number(2),
+    str: new String(2),
+    symbol: Object(Symbol(1)),
+    date: new Date(),
+    reg: /\d+/,
+    error: new Error(),
+    func1: () => {
+        console.log('code秘密花园');
+    },
+    func2: function (a, b) {
+        return a + b;
+    }
+};
+const cloneObj = clone(target)
+target.reg = /1231/
+console.log(cloneObj)
---
+++ HTMLLearn/高阶函数/正则.js
@@ -0,0 +1,4 @@
+const reg = /abv/
+const str = 'abv'
+const res = reg.exec(str)
+console.log(res)
