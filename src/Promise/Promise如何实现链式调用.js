a96e282159b90497b012525d62aeb99bf4d62c7f
lhl <1244224501@qq.com>
2024-11-14 23:10:49 +0800

    fix: 只能调用最后一次函数

--- src/Promise/Promise如何实现链式调用.js
+++ src/Promise/Promise如何实现链式调用.js
@@ -1,10 +1,9 @@
-
 // 定义三种状态
+const fs = require("fs");
 
-const PENDING = "pending"
-const FULFILLED = "fulfilled"
-const REJECTED = "rejected"
-
+const PENDING = "pending";
+const FULFILLED = "fulfilled";
+const REJECTED = "rejected";
 
 /**
  * 这是一个手写的Promise
@@ -13,54 +12,75 @@ const REJECTED = "rejected"
  * 3. 当调用resolve时， 需要先判断当前状态是否是 pending， 如果不是 pending 直接 return  如果是pending,改变 status，调用onFulfilled。
  * 4. 拒绝状态下也同理。
  */
-function MyPromise(executor){
-    let self = this
-    self.value = null
-    self.error = null
-    self.status = PENDING
-    self.onFulfilled = null
-    self.onRejected = null
-    const resolve = (value)=>{
-        if(self.status !== PENDING){
-            return
-        }
-        setTimeout(()=>{
-            self.value = value
-            self.status = FULFILLED
-            if(typeof self.onFulfilled === 'function'){
-                self.onFulfilled(self.value)
-            }
-        },0)
+function MyPromise(executor) {
+  let self = this;
+  self.value = null;
+  self.error = null;
+  self.status = PENDING;
+  self.onFulfilled = null;
+  self.onRejected = null;
+  const resolve = (value) => {
+    if (self.status !== PENDING) {
+      return;
     }
+    setTimeout(() => {
+      self.value = value;
+      self.status = FULFILLED;
+      if (typeof self.onFulfilled === "function") {
+        self.onFulfilled(self.value);
+      }
+    }, 0);
+  };
 
-    const reject = (error)=>{
-        if(self.status !== PENDING){
-            return
-        }
-        setTimeout(() => {
-            self.error = error
-            self.status = REJECTED
-            if(typeof self.onRejected === 'function'){
-                self.onRejected(self.error)
-            }
-        }, 0);
+  const reject = (error) => {
+    if (self.status !== PENDING) {
+      return;
     }
-    executor(resolve,reject)
+    setTimeout(() => {
+      self.error = error;
+      self.status = REJECTED;
+      if (typeof self.onRejected === "function") {
+        self.onRejected(self.error);
+      }
+    }, 0);
+  };
+  executor(resolve, reject);
 }
 
 // 在一个函数的 prototype 上绑定一个 then 方法，通过 then 进行链式调用
-MyPromise.prototype.then = function (onFulfilled,onRejected){
-    // 如果是pending状态，先给回调赋值
-    if(this.status === PENDING){
-        this.onFulfilled = onFulfilled
-        this.onRejected = onRejected
+MyPromise.prototype.then = function (onFulfilled, onRejected) {
+  // 如果是pending状态，先给回调赋值
+  if (this.status === PENDING) {
+    this.onFulfilled = onFulfilled;
+    this.onRejected = onRejected;
+  }
+  // 如果是fulfilled 直接调用成功回调，并将成功值返回
+  if (this.status === FULFILLED) {
+    onFulfilled(this.value);
+  }
+  // 如果是rejected，直接执行失败回调，并将失败值返回。
+  if (this.status === REJECTED) {
+    onRejected(this.value);
+  }
+};
+
+let promise1 = new MyPromise((resolve, reject) => {
+  fs.readFile("./001.txt", (err, data) => {
+    if (!err) {
+      resolve(data);
+    } else {
+      reject(err);
     }
-    // 如果是fulfilled 直接调用成功回调，并将成功值返回
-    if(this.status === FULFILLED){
-        onFulfilled(this.value)
-    }
-    // 如果是rejected，直接执行失败回调，并将失败值返回。
-    if(this.status === REJECTED){
-        onRejected(this.value)
-    }
-}
+  });
+});
+let x1 = promise1.then((data) => {
+  console.log("第一次展示", data.toString());
+});
+
+let x2 = promise1.then((data) => {
+  console.log("第二次展示", data.toString());
+});
+
+let x3 = promise1.then((data) => {
+  console.log("第三次展示", data.toString());
+});
