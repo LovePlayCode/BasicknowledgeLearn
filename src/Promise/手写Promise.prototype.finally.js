4ce60403ca9b3ffcdf487353d0efbb6b5d6411a7
lhl <1244224501@qq.com>
2024-11-16 16:24:20 +0800

    feat: promise

--- src/Promise/手写Promise.prototype.finally.js
+++ src/Promise/手写Promise.prototype.finally.js
@@ -1,14 +1,18 @@
 /**
  * finally 没有接收入参，所以获取不到返回值，
+ * 当finally返回了一个错误，会覆盖原来 Pormise 的错误。
  */
 Promise.prototype.finally = function (callback) {
   return this.then(
     (value) => {
+      console.log("error==");
       return Promise.resolve(callback()).then(() => {
+        console.log("error2==");
         return value;
       });
     },
     (error) => {
+      console.log("error==", error);
       return Promise.resolve(callback()).then(() => {
         throw error;
       });
@@ -24,5 +28,5 @@ Promise.resolve("success")
     console.log("res==", res);
   })
   .catch((res) => {
-    console.log("res==", res);
+    console.log("resError==", res);
   });
